0ccea63da37ebd78e9611689a9f915506cff2537
SpecialBread12 <78811230+SpecialBread12@users.noreply.github.com>
2025-03-26 16:06:58 -0400

    ajustement

--- test-gpio.py
+++ test-gpio.py
@@ -1,18 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-# Configurer le mode de numérotation des GPIO
+# Configuration du mode des GPIO
 GPIO.setmode(GPIO.BCM)
 
-# Configurer le GPIO 17 comme sortie (utilise un autre GPIO si nécessaire)
-GPIO.setup(17, GPIO.OUT)
+# GPIO utilisé pour la commande du transistor
+GPIO_LED = 17
 
-# Allumer la LED pendant 1 seconde
-GPIO.output(17, GPIO.HIGH)
+# Configurer le GPIO comme sortie
+GPIO.setup(GPIO_LED, GPIO.OUT)
+
+# Éteindre la LED (mettre la base du transistor à HIGH)
+GPIO.output(GPIO_LED, GPIO.HIGH)
 time.sleep(1)
 
-# Éteindre la LED
-GPIO.output(17, GPIO.LOW)
+# Allumer la LED (mettre la base du transistor à LOW)
+GPIO.output(GPIO_LED, GPIO.LOW)
+time.sleep(1)
 
-# Nettoyer les configurations GPIO
+# Nettoyer la configuration des GPIO
 GPIO.cleanup()
